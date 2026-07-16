072743610d0d02d2be59ba68f300a0557435dd8f
Arpith622 <arpith.raj.622@gmail.com>
2026-07-16 11:35:08 +0530

    Day 1: Complete Problem - type-awareness

---
+++ day-01-variables-data-types/05-type-awareness.js
@@ -0,0 +1,25 @@
+const num = 666;
+const strNum = "666";
+const isHuman = true;
+const myStomach = null;
+let myMeal;
+
+console.log(`Value : ${num}
+Type : ${typeof num}
+`);
+
+console.log(`Value : ${strNum}
+Type : ${typeof strNum}
+`);
+
+console.log(`Value : ${isHuman}
+Type : ${typeof isHuman}
+`);
+
+console.log(`Value : ${myStomach}
+Type : ${typeof myStomach}
+`);
+
+console.log(`Value : ${myMeal}
+Type : ${typeof myMeal}
+`);
