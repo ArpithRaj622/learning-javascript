a5619492ea30cc446417f615d1cd6d5bfc300974
Arpith622 <arpith.raj.622@gmail.com>
2026-07-16 11:27:14 +0530

    Day 1: Complete Problem - data-type-detective.js

---
+++ day-01-variables-data-types/02-data-type-detective.js
@@ -0,0 +1,28 @@
+const myName = "Arpith";
+const age = 24;
+const isDeveloper = true;
+let city;
+const job = null;
+const salary = 9999999999999n;
+const id = Symbol("id");
+
+console.log(`Value - ${myName}, Type - ${typeof myName}
+`);
+
+console.log(`Value - ${age}, Type - ${typeof age}
+`);
+
+console.log(`Value - ${isDeveloper}, Type - ${typeof isDeveloper}
+`);
+
+console.log(`Value - ${city}, Type - ${typeof city}
+`);
+
+console.log(`Value - ${job}, Type - ${typeof job}
+`);
+
+console.log(`Value - ${salary}, Type - ${typeof salary}
+`);
+
+console.log("Value - ",id);
+console.log(`Type - ${typeof id}`);
