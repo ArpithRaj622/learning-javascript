1a5877db82bc6f871ce6ce43761809b9d9c21a8c
Arpith622 <arpith.raj.622@gmail.com>
2026-07-16 11:30:27 +0530

    Day 1: Complete Problem - variable-reassignment.js

---
+++ day-01-variables-data-types/03-variable-reassignment.js
@@ -0,0 +1,18 @@
+const fullName = "Arpith Raj";
+let age = 24;
+let city = "Raichur";
+
+console.log("Before Re-assign");
+console.log(`User Name - ${fullName}
+Age - ${age}
+City - ${city}`);
+
+console.log();
+
+age = 25;
+city = "Bengaluru";
+
+console.log("After Re-assign");
+console.log(`User Name - ${fullName}
+Age - ${age}
+City - ${city}`);
