daa8fd62131f819ef23e73816decdc203f862200
Arpith622 <arpith.raj.622@gmail.com>
2026-07-16 11:32:42 +0530

    Day 1: Complete Problem - user-profile.js

---
+++ day-01-variables-data-types/04-user-profile.js
@@ -0,0 +1,13 @@
+const fullName = "Arpith Raj"
+const age = 24;
+const email = "arpith.raj.622@gmail.com";
+const phoneNumber = "+91-8660271330";
+const accountVerify = true;
+const memberType = "Diamond";
+
+console.log(`Name : ${fullName}
+Age : ${age}
+Email : ${email}
+Phone : ${phoneNumber}
+Account Verified : ${accountVerify}
+Membership : ${memberType}`);
