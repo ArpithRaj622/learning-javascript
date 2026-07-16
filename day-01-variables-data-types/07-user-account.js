c4a74fab4925c0172f5f7f5c06891fd3acf62ca5
Arpith622 <arpith.raj.622@gmail.com>
2026-07-16 11:38:21 +0530

    Day 1: Complete Problem - user-account.js

---
+++ day-01-variables-data-types/07-user-account.js
@@ -0,0 +1,20 @@
+const userId = 666222;
+const fullName = "Arpith Raj";
+const age = 24;
+const email = "arpith.raj.622@gmail.com";
+const accountBalance = 6666666666;
+const isAccountActive = true;
+const lastLoginDate = "14-07-2026";
+const userLanguage = "English";
+
+
+console.log(`User Id : ${userId}
+Full Name : ${fullName}
+Age : ${age}
+Email : ${email}
+Account Balance : ${accountBalance}
+Account Active Status : ${isAccountActive}
+Last Login Date : ${lastLoginDate}
+User's Preferred Language : ${userLanguage}`);
+
+// "Continue JavaScript practice. We finished Day 1 Variables & Data Types. Start Day 2 Operators."
