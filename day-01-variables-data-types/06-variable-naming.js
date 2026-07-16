2b12bef4af5b8ce762ee7f70338ae4ec7868ed5a
Arpith622 <arpith.raj.622@gmail.com>
2026-07-16 11:36:36 +0530

    Day 1: Complete Problem - variable-naming.js

---
+++ day-01-variables-data-types/06-variable-naming.js
@@ -0,0 +1,11 @@
+const projectName = "Majestic Thota";
+const completedFeatures = 6;
+const isProjectDeployed = false;
+const projectDescription = "blah blah blah";
+const techStack = "MERN Stack";
+
+console.log(`Project Name : ${projectName}
+Features Completed : ${completedFeatures}
+Project Deployed : ${isProjectDeployed}
+Project Description : ${projectDescription}
+Technology Stack : ${techStack}`);
