95a656ef18762632994d18a0bc4a7eb15b9ba0c6
Arpith Raj <arpith.raj.622@gmail.com>
2026-07-16 11:16:13 +0530

    Day 1: Complete Problem - basic-info.js

---
+++ day-01-variables-data-types/01-basic-info.js
@@ -0,0 +1,10 @@
+const fullName = "Arpith Raj";
+const age = 24;
+const favoriteLanguage = "JavaScript";
+let enjoyCoding = true;
+
+
+console.log(`Name : ${fullName}
+Age : ${age}
+Favorite Language : ${favoriteLanguage}
+Enjoys Coding: ${enjoyCoding}`);
