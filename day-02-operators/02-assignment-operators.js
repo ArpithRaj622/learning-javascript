eb8952e7a007b44222689916cd002bb59144e2ec
Arpith622 <arpith.raj.622@gmail.com>
2026-07-17 20:29:54 +0530

    Complete assignment operators

---
+++ day-02-operators/02-assignment-operators.js
@@ -0,0 +1,29 @@
+let num = 999;
+
+num = 666;
+console.log(`num = ${num}
+`);
+
+// num += 2 -> num = num + 2
+console.log(`${num} += 2 -> ${num+=2}
+`);
+
+// num -= 2 -> num = num - 2
+console.log(`${num} -= 2 -> ${num-=2}
+`);
+
+// num *= 2 -> num = num * 2
+console.log(`${num} *= 2 -> ${num*=2}
+`);
+
+// num /= 2 -> num = num / 2
+console.log(`${num} /= 2 -> ${num/=2}
+`);
+
+// num %= 4 -> num = num % 4
+console.log(`${num} %= 4 -> ${num%=4}
+`);
+
+// num **= 2 -> num = num ** 2
+console.log(`${num} **= 2 -> ${num**=2}
+`);
