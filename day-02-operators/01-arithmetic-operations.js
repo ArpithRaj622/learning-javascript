2f301bab665a9e409b5fbcc381b522801598f974
Arpith622 <arpith.raj.622@gmail.com>
2026-07-17 20:06:09 +0530

    Complete arithmetic operators

---
+++ day-02-operators/01-arithmetic-operations.js
@@ -0,0 +1,34 @@
+const num1 = 66;
+const num2 = 99;
+
+// addition
+console.log(`Addition:
+${num1} + ${num2} = ${num1+num2}
+`);
+
+// subtraction
+console.log(`Subtraction:
+${num2} - ${num1} = ${num2-num1}
+`);
+
+// multiplication
+console.log(`Multiplication:
+${num1} * ${num2} = ${num1*num2}
+`);
+
+// division
+console.log(`Division:
+${num2} / ${num1} = ${num2/num1}
+`);
+
+// modulus
+console.log(`Modulus (remainder):
+${num2} % ${num1} = ${num2%num1}
+`);
+
+// exponentiation
+console.log(`Exponentiation:
+${num1} ** 2 = ${num1**2}
+${num2} ** 3 = ${num2**3}
+${num2} ** ${num1} = ${num2**num1}
+`);
