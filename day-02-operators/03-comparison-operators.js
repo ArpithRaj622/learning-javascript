ac63a1eff4a9734ed931e1408822d208cf99125c
Arpith622 <arpith.raj.622@gmail.com>
2026-07-17 20:57:07 +0530

    Complete comparision operators

---
+++ day-02-operators/03-comparison-operators.js
@@ -0,0 +1,35 @@
+const age = 24;
+const marks = "24";
+const passwordLength = 8;
+const height = 177;
+
+// equal to
+console.log(`${age} == "${marks}" -> ${age == marks}
+`);
+
+// strict equal to
+console.log(`${age} === "${marks}" -> ${age === marks}
+`);
+
+// not equal to
+console.log(`${age} != "${marks}" -> ${age != marks}
+`);
+
+// strict not equal to
+console.log(`${age} !== "${marks}" -> ${age !== marks}
+`);
+
+// greater than
+console.log(`passwordLength(${passwordLength}) > 8 -> ${passwordLength>8}
+`);
+
+// less than
+console.log(`passwordLength(${passwordLength}) < 8 -> ${passwordLength<8}
+`);
+
+// greater than or equal to
+console.log(`passwordLength(${passwordLength}) >= 8 -> ${passwordLength>=8}
+`);
+
+// less than or equal to
+console.log(`height(177) <= 180 -> ${height<=180}`);
