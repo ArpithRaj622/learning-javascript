475912afe95b5462c170547b0d234844cc8fc02d
Arpith622 <arpith.raj.622@gmail.com>
2026-07-17 23:04:21 +0530

    Complete increment-decrement

---
+++ day-02-operators/06-increment-decrement.js
@@ -0,0 +1,27 @@
+let num = 666;
+
+// pre-increment
+console.log(`Pre-increment:
+Before increment num = ${num}
+After increment ++num = ${++num}
+`);
+
+// post-increment
+console.log(`Post-increment:
+Before increment num = ${num}
+After increment num++ = ${num++}
+After the above line execution, num = ${num}
+`);
+
+// pre-decrement
+console.log(`Pre-decrement:
+Before decrement num = ${num}
+After decrement --num = ${--num}
+`);
+
+// post-decrement
+console.log(`Post-decrement:
+Before decrement num = ${num}
+After decrement num-- = ${num--}
+After the above line execution, num = ${num} 
+`);
