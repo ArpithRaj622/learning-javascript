5770ea1efc1cda839d0b13b62888dc1dc202986e
Arpith622 <arpith.raj.622@gmail.com>
2026-07-19 23:58:39 +0530

    Complete movie ticket price

---
+++ day-04-conditional-challenges/06-movie-ticket-price.js
@@ -0,0 +1,11 @@
+const age = 65;
+
+if (age < 5) {
+    console.log("Free Entry");
+} else if (age <= 12) {
+    console.log("Ticket Price: ₹100");
+} else if (age <= 59) {
+    console.log("Ticket Price: ₹200");
+} else {
+    console.log("Ticket Price: ₹150");
+}
