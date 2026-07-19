02c3cfdac45584e354d50e5463c217c23561a000
Arpith622 <arpith.raj.622@gmail.com>
2026-07-19 23:53:09 +0530

    Complete traffic light

---
+++ day-04-conditional-challenges/05-traffic-light.js
@@ -0,0 +1,11 @@
+const signal = "green";
+
+if (signal === "red") {
+    console.log("Stop");
+} else if (signal === "yellow") {
+    console.log("Get ready");
+} else if (signal === "green") {
+    console.log("Go");
+} else {
+    console.log("Invalid Signal");
+}
