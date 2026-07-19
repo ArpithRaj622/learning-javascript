775ca1fde7fb249e39470088ef12d392b6be583e
Arpith622 <arpith.raj.622@gmail.com>
2026-07-19 23:41:24 +0530

    Complete bmi category

---
+++ day-04-conditional-challenges/03-bmi-category.js
@@ -0,0 +1,11 @@
+const bmi = 18;
+
+if (bmi >= 30) {
+    console.log("Obese");
+}  else if (bmi >= 25) {
+    console.log("Overweight");
+} else if (bmi >= 18.5) {
+    console.log("Normal weight");
+} else {
+    console.log("Underweight");
+}
