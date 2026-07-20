3e2769a60dfe195700bc45dcae11b8a04600a94f
Arpith622 <arpith.raj.622@gmail.com>
2026-07-20 21:10:35 +0530

    Complete number sign

---
+++ day-05-conditional-challenges-2/01-number-sign.js
@@ -0,0 +1,11 @@
+const number = 6;
+
+if (number < 0) {
+    console.log("Negative");
+} else if (number === 0) {
+    console.log("Zero");
+} else if (number % 2 === 0) {
+    console.log("Positive Even");
+} else {
+    console.log("Positive Odd");
+}
