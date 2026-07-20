e23160925fd54517100d89c22358c954bb022357
Arpith622 <arpith.raj.622@gmail.com>
2026-07-20 21:15:49 +0530

    Complete largest number

---
+++ day-05-conditional-challenges-2/02-largest-of-three.js
@@ -0,0 +1,11 @@
+const num1 = 22;
+const num2 = 48;
+const num3 = 66;
+
+if (num1 > num2 && num1 > num3) {
+    console.log("num1 is the largest");
+} else if (num2 > num3) {
+    console.log("num2 is the largest");
+} else {
+    console.log("num3 is the largest");
+}
