8df0faef7518924feaf42aec3190c5ed9de1a75b
Arpith622 <arpith.raj.622@gmail.com>
2026-07-20 21:53:06 +0530

    Complete exam result

---
+++ day-05-conditional-challenges-2/06-exam-result.js
@@ -0,0 +1,9 @@
+const marks = 96;
+const attendance = 76;
+const hasCheated = false;
+
+if (marks >= 40 && attendance >= 75 && !hasCheated) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
