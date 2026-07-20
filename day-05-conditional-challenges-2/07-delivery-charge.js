70e7ff9f06c4024c625676d25561365f89104d5f
Arpith622 <arpith.raj.622@gmail.com>
2026-07-20 21:58:48 +0530

    Complete delivery charge

---
+++ day-05-conditional-challenges-2/07-delivery-charge.js
@@ -0,0 +1,8 @@
+const orderAmount = 1000;
+const isPrimeMember = false;
+
+if (orderAmount >= 1000 || isPrimeMember) {
+    console.log("Free Delivery");
+} else {
+    console.log("Delivery Charge: ₹99");
+}
