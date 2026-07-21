8d0f604f350712cbbbed26fa8cb2c14fc7a51dc7
Arpith622 <arpith.raj.622@gmail.com>
2026-07-21 16:15:41 +0530

    Complete login validation

---
+++ day-06-conditional-challenges-3/01-login-validation.js
@@ -0,0 +1,9 @@
+const username = "arpith";
+const password = "javascript123";
+const isBlocked = false;
+
+if (username === "arpith" && password === "javascript123" && !isBlocked) {
+    console.log("Login Successful");
+} else {
+    console.log("Login Failed");
+}
