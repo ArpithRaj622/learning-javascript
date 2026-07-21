88aeccc4cf136b36eae100e7f27a8b1e777a89f3
Arpith622 <arpith.raj.622@gmail.com>
2026-07-21 23:41:11 +0530

    Complete event registration

---
+++ day-06-conditional-challenges-3/05-event-registration.js
@@ -0,0 +1,9 @@
+const age = 24;
+const hasValidId = true;
+const registrationFeePaid = true;
+
+if (age >= 18 && hasValidId && registrationFeePaid) {
+    console.log("Registration Successful");
+} else {
+    console.log("Registration Failed");
+}
