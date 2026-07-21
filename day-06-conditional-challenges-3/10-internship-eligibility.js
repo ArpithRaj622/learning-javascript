ad036817028e13fbdb2f4b40f113c7cb3bd302f7
Arpith622 <arpith.raj.622@gmail.com>
2026-07-22 00:03:46 +0530

    Complete internship eligibility

---
+++ day-06-conditional-challenges-3/10-internship-eligibility.js
@@ -0,0 +1,10 @@
+const age = 24;
+const graduated = true;
+const knowsJavaScript = true;
+const hasResume = true;
+
+if (age >=18 && graduated && knowsJavaScript && hasResume) {
+    console.log("Eligibile for Internship");
+} else {
+    console.log("Not Eligible");
+}
