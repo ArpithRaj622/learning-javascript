c4dc4082ad46668ba943058683e17ad8b00918fe
Arpith622 <arpith.raj.622@gmail.com>
2026-07-21 16:45:24 +0530

    Complete scholarship elibibility

---
+++ day-06-conditional-challenges-3/03-scholarship-eligibility.js
@@ -0,0 +1,9 @@
+const marks = 96;
+const annualIncome = 300000;
+const hasDisciplinaryAction = false;
+
+if (marks >= 85 && annualIncome <= 300000 && !hasDisciplinaryAction) {
+    console.log("Scholarship Approved");
+} else {
+    console.log("Scholarship Rejected");
+}
