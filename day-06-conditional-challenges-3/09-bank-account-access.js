6c93b6b844917dc7e806bed21a59da613d64b9e9
Arpith622 <arpith.raj.622@gmail.com>
2026-07-21 23:58:24 +0530

    Complete bank account access

---
+++ day-06-conditional-challenges-3/09-bank-account-access.js
@@ -0,0 +1,9 @@
+const isAccountActive = true;
+const hasSufficientBalance = true;
+const pinVerified = true;
+
+if (isAccountActive && hasSufficientBalance && pinVerified) {
+    console.log("Transaction Allowed");
+} else {
+    console.log("Transaction Denied");
+}
