9723ce8bcc2cab0be0a9a1120b9577efc3b8d9e0
Arpith622 <arpith.raj.622@gmail.com>
2026-07-21 23:54:35 +0530

    Complete gym membership access

---
+++ day-06-conditional-challenges-3/08-gym-membership-access.js
@@ -0,0 +1,9 @@
+const age = 24;
+const membershipActive = true;
+const medicalCertificate = true;
+
+if (age >= 18 && membershipActive && medicalCertificate) {
+    console.log("Gym Access Granted");
+} else {
+    console.log("Gym Access Denied");
+}
