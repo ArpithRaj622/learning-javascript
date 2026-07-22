ec77e750f0b138ae9a998de49e9263281c375866
Arpith622 <arpith.raj.622@gmail.com>
2026-07-22 23:01:49 +0530

    Complete pizza size

---
+++ day-07-switch-statement/09-pizza-size.js
@@ -0,0 +1,18 @@
+const size = "L";
+
+switch (size) {
+    case "S":
+        console.log("Small Pizza");
+        break;
+    case "M":
+        console.log("Medium Pizza");
+        break;
+    case "L":
+        console.log("Large Pizza");
+        break;
+    case "XL":
+        console.log("Extra Large Pizza");
+        break;
+    default:
+        console.log("Invalid Size");
+}
