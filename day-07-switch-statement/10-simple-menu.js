6a582f0da7138a2698f52bd25337824d177991e8
Arpith622 <arpith.raj.622@gmail.com>
2026-07-22 23:07:16 +0530

    Complete simple menu

---
+++ day-07-switch-statement/10-simple-menu.js
@@ -0,0 +1,18 @@
+const choice = 1;
+
+switch (choice) {
+    case 1:
+        console.log("View Profile");
+        break;
+    case 2:
+        console.log("Edit Profile");
+        break;
+    case 3:
+        console.log("Settings");
+        break;
+    case 4:
+        console.log("Logout");
+        break;
+    default:
+        console.log("Invalid choice");
+}
